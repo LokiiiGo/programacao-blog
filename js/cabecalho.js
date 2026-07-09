1839ddc69bf2adb0282ad7b9ab760a6488e22bca
LokiiiGo <lucascalle880@gmail.com>
2026-07-09 20:23:49 -0300

    Alterando para o ícone list do framework bootstrap

--- js/cabecalho.js
+++ js/cabecalho.js
@@ -16,8 +16,8 @@ if (headerEl) {
         aria-label="Abrir menu"
         aria-expanded="false"
       >
-        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
         </svg>
       </button>
 
